e22acd8ee1868701a341cba5387eb4400265f288
fmoletta <fedemoletta@hotmail.com>
2025-12-18 16:14:38 -0300

    fix

--- bin/cli/src/main.rs
+++ bin/cli/src/main.rs
@@ -1,10 +1,11 @@
-use vm::{elf::Elf, vm::execution::run_program};
+use executor::{elf::Elf, vm::execution::run_program};
 
 fn main() {
     let mut args = std::env::args();
     let elf_filename = args.nth(1).expect("No filename given");
     let elf_data = std::fs::read(elf_filename).expect("Failed to read elf file");
     let program = Elf::load(&elf_data).expect("Failed to load elf program");
-    let (logs, _) = run_program(program.image, program.entry_point).expect("Failed to run program");
-    // TODO: Prove program
+    let (_logs, _) =
+        run_program(program.image, program.entry_point).expect("Failed to run program");
+    // TODO: Prove program execution using logs
 }
