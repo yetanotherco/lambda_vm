d3ed51bd3f96df9cc283bd479cbfc83c344076ef
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-12-19 18:31:14 -0300

    Format code

--- executor/src/vm/instruction/decoding.rs
+++ executor/src/vm/instruction/decoding.rs
@@ -393,9 +393,7 @@ fn parse_i_instruction(instruction: u32, opcode: Opcode) -> Result<Instruction, 
                     dst: rs1,
                     op: CsrOp::CSRRW,
                 },
-                CSRRC_FUNC_IDENTIFIER
-                | CSRRWI_FUNC_IDENTIFIER
-                | CSRRSI_FUNC_IDENTIFIER => {
+                CSRRC_FUNC_IDENTIFIER | CSRRWI_FUNC_IDENTIFIER | CSRRSI_FUNC_IDENTIFIER => {
                     // For now, we do not support these CSR instructions
                     return Err(InstructionError::InvalidSystemInstruction(func3));
                 }
