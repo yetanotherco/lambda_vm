fafaa43c25409fb61bb676dc605d01a1e875fa96
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-12-19 12:12:28 -0300

    Use lazy static

--- prover/src/air/utils.rs
+++ prover/src/air/utils.rs
@@ -3,26 +3,29 @@ use lambdaworks_math::field::fields::fft_friendly::{
 };
 use stark_platinum_prover::{fri::FieldElement, table::TableView};
 
+lazy_static::lazy_static! {
+    pub static ref TWO_FIFTY_SIX: FieldElement<Babybear31PrimeField> =
+        FieldElement::<Babybear31PrimeField>::from(256);
+}
+
 pub(crate) fn compute_element_from_two_limbs_starting_at(
     step: &TableView<Babybear31PrimeField, Degree4BabyBearU32ExtensionField>,
     index: usize,
 ) -> FieldElement<Babybear31PrimeField> {
-    let two_fifty_six = FieldElement::<Babybear31PrimeField>::from(256);
     step.get_main_evaluation_element(0, index)
-        + two_fifty_six * step.get_main_evaluation_element(0, index + 1)
+        + *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 1)
 }
 
 pub(crate) fn compute_element_from_four_limbs_starting_at(
     step: &TableView<Babybear31PrimeField, Degree4BabyBearU32ExtensionField>,
     index: usize,
 ) -> FieldElement<Babybear31PrimeField> {
-    let two_fifty_six = FieldElement::<Babybear31PrimeField>::from(256);
     step.get_main_evaluation_element(0, index)
-        + two_fifty_six * step.get_main_evaluation_element(0, index + 1)
-        + two_fifty_six * two_fifty_six * step.get_main_evaluation_element(0, index + 2)
-        + two_fifty_six
-            * two_fifty_six
-            * two_fifty_six
+        + *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 1)
+        + *TWO_FIFTY_SIX * *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 2)
+        + *TWO_FIFTY_SIX
+            * *TWO_FIFTY_SIX
+            * *TWO_FIFTY_SIX
             * step.get_main_evaluation_element(0, index + 3)
 }
 
@@ -30,21 +33,19 @@ pub(crate) fn compute_element_from_two_limbs_starting_at_extension(
     step: &TableView<Degree4BabyBearU32ExtensionField, Degree4BabyBearU32ExtensionField>,
     index: usize,
 ) -> FieldElement<Degree4BabyBearU32ExtensionField> {
-    let two_fifty_six = FieldElement::<Babybear31PrimeField>::from(256);
     step.get_main_evaluation_element(0, index)
-        + two_fifty_six * step.get_main_evaluation_element(0, index + 1)
+        + *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 1)
 }
 
 pub(crate) fn compute_element_from_four_limbs_starting_at_extension(
     step: &TableView<Degree4BabyBearU32ExtensionField, Degree4BabyBearU32ExtensionField>,
     index: usize,
 ) -> FieldElement<Degree4BabyBearU32ExtensionField> {
-    let two_fifty_six = FieldElement::<Babybear31PrimeField>::from(256);
     step.get_main_evaluation_element(0, index)
-        + two_fifty_six * step.get_main_evaluation_element(0, index + 1)
-        + two_fifty_six * two_fifty_six * step.get_main_evaluation_element(0, index + 2)
-        + two_fifty_six
-            * two_fifty_six
-            * two_fifty_six
+        + *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 1)
+        + *TWO_FIFTY_SIX * *TWO_FIFTY_SIX * step.get_main_evaluation_element(0, index + 2)
+        + *TWO_FIFTY_SIX
+            * *TWO_FIFTY_SIX
+            * *TWO_FIFTY_SIX
             * step.get_main_evaluation_element(0, index + 3)
 }
