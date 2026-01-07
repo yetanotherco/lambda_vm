d815f98fcb59aee025bf328083f375818e90fe6a
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2026-01-07 15:14:05 -0300

    Format

--- syscalls/src/entrypoint.rs
+++ syscalls/src/entrypoint.rs
@@ -1,7 +1,7 @@
 use crate::{allocator::init_allocator, syscalls::sys_halt};
 
 /// # Safety
-/// 
+///
 /// This function is the default entrypoint and should not be called directly.
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn _start() -> ! {
