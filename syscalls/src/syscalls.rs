0df5d02aad1219d9669127864be86f681eececbe
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2026-01-08 15:30:53 -0300

    Remove atomics definitions

--- syscalls/src/syscalls.rs
+++ syscalls/src/syscalls.rs
@@ -1,5 +1,4 @@
 use core::arch::asm;
-use core::ptr;
 
 // TODO: This should be properly defined
 const MAX_PRIVATE_INPUT_SIZE: usize = 1024;
@@ -58,146 +57,6 @@ pub unsafe extern "C" fn sys_panic(msg_ptr: *const u8, len: usize) {
     }
 }
 
-fn load_64(ptr: *const u8) -> u64 {
-    unsafe {
-        let mut v: u64 = 0;
-        for i in 0..8 {
-            v |= (*ptr.add(i) as u64) << (i * 8);
-        }
-        v
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub extern "C" fn __atomic_load_8(ptr: *const u64, _order: i32) -> u64 {
-    print_string("__atomic_load_8 called\n");
-    if ptr.is_null() {
-        return 0;
-    }
-    let p = ptr as *const u8;
-
-    load_64(p)
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn __atomic_fetch_add_4(ptr: *mut u32, val: u32, _order: i32) -> u32 {
-    print_string("__atomic_fetch_add_4 called\n");
-    unsafe {
-        let old = *ptr;
-        *ptr = old.wrapping_add(val);
-        old
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub extern "C" fn __atomic_fetch_and_1(ptr: *mut u8, val: u8, _order: i32) -> u8 {
-    print_string("__atomic_fetch_and_1 called\n");
-    unsafe {
-        let old = *ptr;
-        *ptr = old & val;
-        old
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub extern "C" fn __atomic_fetch_sub_4(ptr: *mut u32, val: u32, _order: i32) -> u32 {
-    print_string("__atomic_fetch_sub_4 called\n");
-    unsafe {
-        let old = *ptr;
-        *ptr = old.wrapping_sub(val);
-        old
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn __atomic_load_4(ptr: *const u32, _order: i32) -> u32 {
-    print_string("__atomic_load_4 called\n");
-    unsafe { *ptr }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn __atomic_load_1(ptr: *const u8, _order: i32) -> u8 {
-    print_string("__atomic_load_1 called\n");
-    unsafe { *ptr }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub extern "C" fn __atomic_store_1(ptr: *mut u8, val: u8, _order: i32) {
-    print_string("__atomic_store_1 called\n");
-    unsafe {
-        *ptr = val;
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn __atomic_fetch_or_1(ptr: *mut u8, val: u8, _order: i32) -> u8 {
-    print_string("__atomic_fetch_or_1 called\n");
-    unsafe {
-        let old = *ptr;
-        *ptr = old | val;
-        old
-    }
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn __atomic_compare_exchange_8(
-    ptr: *mut u64,
-    expected: *mut u64,
-    desired: u64,
-    _weak: bool,
-    _success_order: i32,
-    _failure_order: i32,
-) -> bool {
-    print_string("__atomic_compare_exchange_8 called\n");
-    unsafe {
-        let current = load_64(ptr as *const u8);
-        let expected_val = load_64(expected as *const u8);
-        if current == expected_val {
-            ptr::write_unaligned(ptr, desired);
-            true
-        } else {
-            ptr::write_unaligned(expected, current);
-            false
-        }
-    }
-}
-
 /// # Safety
 ///
 /// This function should not be called by the user
@@ -206,18 +65,6 @@ pub unsafe extern "C" fn __atomic_compare_exchange_8(
 pub extern "C" fn sys_rand(_buf: *mut u8, _len: usize) -> isize {
     print_string("sys_rand called\n");
     0
-}
-
-/// # Safety
-///
-/// This function should not be called by the user
-/// It is only for rust std internal uses
-#[unsafe(no_mangle)]
-pub extern "C" fn __atomic_store_8(ptr: *mut u64, val: u64, _order: i32) {
-    print_string("__atomic_store_8 called\n");
-    unsafe {
-        ptr::write_unaligned(ptr, val);
-    }
 }
 
 pub fn commit(slice: &[u8]) {
