d52d7153022d43781a67d13c3affb82fbc805091
Diego K <43053772+diegokingston@users.noreply.github.com>
2026-01-12 11:36:16 -0300

    Add fft friendly (#126)
    * add fft parameters for goldilocks
    
    * add tests
    
    * fmt

--- crypto/math/src/field/fields/fft_friendly/u64_goldilocks.rs
+++ crypto/math/src/field/fields/fft_friendly/u64_goldilocks.rs
@@ -2,6 +2,7 @@ use crate::{
     field::{
         element::FieldElement,
         fields::montgomery_backed_prime_fields::{IsModulus, MontgomeryBackendPrimeField},
+        traits::IsFFTField,
     },
     unsigned_integer::element::U64,
 };
@@ -40,5 +41,25 @@ impl PartialOrd for FieldElement<U64GoldilocksPrimeField> {
 impl Ord for FieldElement<U64GoldilocksPrimeField> {
     fn cmp(&self, other: &Self) -> core::cmp::Ordering {
         self.representative().cmp(&other.representative())
+    }
+}
+
+/// IsFFTField implementation for U64GoldilocksPrimeField
+/// The field order is p = 2^64 - 2^32 + 1
+/// p - 1 = 2^64 - 2^32 = 2^32 * (2^32 - 1)
+/// So the two-adicity is 32.
+///
+/// The primitive 2^32-th root of unity was taken from Plonky3:
+/// https://github.com/Plonky3/Plonky3/blob/main/goldilocks/src/lib.rs
+impl IsFFTField for U64GoldilocksPrimeField {
+    const TWO_ADICITY: u64 = 32;
+
+    // 2^32-th primitive root of unity in Montgomery form
+    // From Plonky3: 1753635133440165772
+    // In Montgomery representation for this field
+    const TWO_ADIC_PRIMITVE_ROOT_OF_UNITY: U64 = U64::from_u64(1753635133440165772);
+
+    fn field_name() -> &'static str {
+        "U64Goldilocks"
     }
 }
