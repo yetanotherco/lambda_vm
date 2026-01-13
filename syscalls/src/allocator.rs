57a80f0a7e948b9c6434584a06ce6d14433dae72
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2026-01-13 15:52:02 -0300

    Increase memory

--- syscalls/src/allocator.rs
+++ syscalls/src/allocator.rs
@@ -1,16 +1,17 @@
-use embedded_alloc::LlffHeap as Heap;
+use embedded_alloc::TlsfHeap as Heap;
 use riscv as _;
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
+const MAX_MEMORY_SIZE: usize = 0xC000_0000;
+
 pub fn init_allocator() {
     {
-        use core::mem::MaybeUninit;
-        use core::ptr::addr_of_mut;
-        const HEAP_SIZE: usize = 1024 * 64;
-        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-        let heap_ptr = addr_of_mut!(HEAP_MEM) as *mut u8;
-        unsafe { HEAP.init(heap_ptr as usize, HEAP_SIZE) }
+        unsafe extern "C" {
+            static _end: u8;
+        }
+        let heap_pos: usize = unsafe { (&_end) as *const u8 as usize };
+        unsafe { HEAP.init(heap_pos, MAX_MEMORY_SIZE - heap_pos) }
     }
 }
