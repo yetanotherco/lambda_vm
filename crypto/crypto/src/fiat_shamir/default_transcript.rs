270732511b7cbc43789f77fdb071c5f148594db3
jotabulacios <jbulacios@fi.uba.ar>
2026-03-05 17:40:00 -0300

    fix sample_u64 to advance transcript state

--- crypto/crypto/src/fiat_shamir/default_transcript.rs
+++ crypto/crypto/src/fiat_shamir/default_transcript.rs
@@ -81,7 +81,7 @@ where
     }
 
     fn sample_u64(&mut self, upper_bound: u64) -> u64 {
-        u64::from_be_bytes(self.state()[..8].try_into().unwrap()) % upper_bound
+        u64::from_be_bytes(self.sample()[..8].try_into().unwrap()) % upper_bound
     }
 }
 
