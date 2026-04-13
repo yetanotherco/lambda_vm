c1146e7d4dc8b0aaee2715ac0b033d72640f7d13
gabrielbosio <gabrielbosio95@gmail.com>
2026-04-13 18:52:12 -0300

    Fix stale trace_lde comment in prove_rounds_2_to_4

--- crypto/stark/src/prover.rs
+++ crypto/stark/src/prover.rs
@@ -2086,7 +2086,7 @@ pub trait IsStarkProver<
             let (r4_fft, r4_merkle, r4_deep_comp, r4_queries) =
                 crate::instruments::take_r4_sub().unwrap_or((zero, zero, zero, zero));
             crate::instruments::store_round_sub_ops(crate::instruments::TableSubOps {
-                trace_lde: std::time::Duration::ZERO, // added by caller from lde_dur
+                trace_lde: std::time::Duration::ZERO, // LDE timing in Phase A/C (accum_r1_main/aux)
                 constraints: r2_constraints,
                 comp_decompose: r2_fft,
                 comp_commit: r2_merkle,
