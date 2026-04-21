d768316b12b3083920e6d43fc530abd84d53765f
gabrielbosio <gabrielbosio95@gmail.com>
2026-04-21 13:12:54 -0300

    Revert unrelated fib_iterative_2M tweak

--- executor/programs/asm/fib_iterative_2M.s
+++ executor/programs/asm/fib_iterative_2M.s
@@ -5,11 +5,11 @@ main:
 	# ~2M steps
 	#
 	# Loop body: 5 instructions per iteration
-	# 400000 iterations × 5 = 2000000 + 4 setup/teardown ≈ 2000004
+	# 399999 iterations × 5 = 1999995 + 4 setup/teardown = 1999999
 
 	li	t0, 0			# a = fib(0) = 0
 	li	t1, 1			# b = fib(1) = 1
-	li	a0, 400000		# iteration count
+	li	a0, 399999		# iteration count
 
 .loop:
 	add	t2, t0, t1		# t2 = a + b
@@ -19,6 +19,5 @@ main:
 	bnez	a0, .loop		# loop if n != 0
 
 	mv	a0, t1			# result = b
-	li	a0, 0
 	li	a7, 93
 	ecall				# halt with result in a0
