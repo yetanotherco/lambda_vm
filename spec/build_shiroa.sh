d8ca7f25556157bf9c3a7be4c01ebc9775f9bb35
Robin Jadoul <robin.jadoul@3milabs.tech>
2026-04-23 14:13:22 +0200

    cd into script directory and harden against stale interaction counts

--- spec/build_shiroa.sh
+++ spec/build_shiroa.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
-typst query ebook.typ '<interaction_count>' --field value > interaction_count.json
-shiroa build
+# cd into the script directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Clean up potential old file
 rm -f interaction_count.json
+
+# Always clean up after ourselves
+trap 'rm -f interaction_count.json' EXIT
+
+# Query the ebook version for the proper counts
+typst query ebook.typ '<interaction_count>' --field value > interaction_count.json
+
+# And build
+shiroa build
