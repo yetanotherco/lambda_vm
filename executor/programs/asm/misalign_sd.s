95ca239bcf67dbc06ecdd8dd1b06fd59f144a40d
gabrielbosio <gabrielbosio95@gmail.com>
2026-05-26 19:07:39 -0300

    Verify stored bytes in misalign store tests

--- executor/programs/asm/misalign_sd.s
+++ executor/programs/asm/misalign_sd.s
@@ -5,6 +5,19 @@ main:
 	li	t0, 64
 	li	t1, 0x123456789ABCDEF0
 	sd	t1, 1(t0)
+	lwu	a1, 0(t0)
+	li	t2, 0xBCDEF000
+	bne	a1, t2, .Lfail
+	lwu	a1, 4(t0)
+	li	t2, 0x3456789A
+	bne	a1, t2, .Lfail
+	lwu	a1, 8(t0)
+	li	t2, 0x00000012
+	bne	a1, t2, .Lfail
 	li	a0, 0
+	li	a7, 93
+	ecall
+.Lfail:
+	li	a0, 1
 	li	a7, 93
 	ecall
--- executor/programs/asm/misalign_sw.s
+++ executor/programs/asm/misalign_sw.s
@@ -5,6 +5,16 @@ main:
 	li	t0, 64
 	li	t1, 0xDEADBEEF
 	sw	t1, 1(t0)
+	lwu	a1, 0(t0)
+	li	t2, 0xADBEEF00
+	bne	a1, t2, .Lfail
+	lwu	a1, 4(t0)
+	li	t2, 0x000000DE
+	bne	a1, t2, .Lfail
 	li	a0, 0
+	li	a7, 93
+	ecall
+.Lfail:
+	li	a0, 1
 	li	a7, 93
 	ecall
