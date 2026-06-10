4a8f971a4780d57df94e1cd727d9c2e5fccdec07
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2026-06-10 17:27:28 -0300

    fix print ecall

--- crypto/stark/src/lookup.rs
+++ crypto/stark/src/lookup.rs
@@ -1266,7 +1266,7 @@ impl Multiplicity {
 
     /// Evaluate the multiplicity for a single row of column-major main data.
     #[inline]
-    fn evaluate_at_row<F: IsField>(
+    pub fn evaluate_at_row<F: IsField>(
         &self,
         main_segment_cols: &[Vec<FieldElement<F>>],
         row: usize,
